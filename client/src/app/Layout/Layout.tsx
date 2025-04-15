476818bf861015339a07de3f6c379637b01863f4
ko-tenka <kotelnikova123.123@gmail.com>
2025-04-15 19:55:29 +0300

    release Block css

--- client/src/app/Layout/Layout.tsx
+++ client/src/app/Layout/Layout.tsx
@@ -1,6 +1,5 @@
 import styles from './Layout.module.css';
 import Navbar from '../widgets/Navbar/ui/Navbar';
-import { Sidebar } from '../widgets/SideBar';
 import { Outlet } from 'react-router-dom';
 import BlockSearch from '../../pages/BlockSearch/BlockSearch';
 
@@ -10,7 +9,6 @@ export default function Layout(): JSX.Element {
   return (
     <>
       <Navbar />
-      <Sidebar />
       <BlockSearch/>
       <main className={styles.root}>
         <Outlet />
